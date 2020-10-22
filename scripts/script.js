cdfcb146b9b70a838c45c267aac6ffbaaa6300a8
JakubBombik <jakub.bombik11@gmail.com>
2020-10-22 10:19:58 +0200

    Initial commit
    22.10.2020

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+let video = document.querySelector('.home-video');
+let navigation = document.getElementById("navigation")
+let portfolio = document.querySelectorAll("navigation")
+
+// Homepage Video
+window.addEventListener('scroll' , function(){
+    // change of theme navigation bar
+    navigation.classList.toggle("sticky", window.scrollY > 1);
+    // opacity on homepage
+    let value = 1 + window.scrollY/ -600;
+    video.style.opacity = value;
+    
+})
+// 3D cards
+VanillaTilt.init(document.querySelectorAll(".card"), {
+    max: 25,
+    speed: 400,
+    easing:                 "cubic-bezier(.03,.98,.52,.99)",
+});
+
+// PROJECT GALLERY
+
+
+$('.project').click(function(){
+    $('.backgroun-zoom').toggleClass(('opened'));
+    $(this).toggleClass(('opened'));
+});
+
+
+
+
+
+
+
